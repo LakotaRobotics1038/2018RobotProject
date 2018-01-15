8d56eb69ed7b41d53c247f9853827a37fc671db8
Wesley Reed <reed.wesley.s@gmail.com>
2018-01-15 18:18:15 -0500

    Added Encoder1038 to master

---
+++ src/org/usfirst/frc/team1038/robot/Encoder1038.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team1038.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+public class Encoder1038 {
+	
+	private Encoder encoder;
+	
+	public Encoder1038(int channelA, int channelB, boolean isInverted)
+	{
+		encoder = new Encoder(channelA, channelB, isInverted);
+	}
+	
+	public Encoder1038(int channelA, int channelB, int countsPerRevolution, double wheelDiameter)
+	{
+		encoder = new Encoder(channelA, channelB);
+		encoder.setDistancePerPulse(findDistancePerPulse(countsPerRevolution, wheelDiameter));
+	}
+	
+	public Encoder1038(int channelA, int channelB, boolean isInverted, int countsPerRevolution, double wheelDiameter)
+	{
+		encoder = new Encoder(channelA, channelB, isInverted);
+		encoder.setDistancePerPulse(findDistancePerPulse(countsPerRevolution, wheelDiameter));
+	}
+	
+	public void resetEncoder() {
+		encoder.reset();
+	}
+	
+	public int getCount()
+	{
+		return encoder.get();
+	}
+	
+	public double getRate()
+	{
+		return encoder.getRate();
+	}
+	
+	public double getDistance()
+	{
+		return encoder.getDistance();
+	}
+	
+	private double findDistancePerPulse(double countsPerRevolution, double wheelDiameter)
+	{		
+		return (1 / countsPerRevolution ) * (wheelDiameter * Math.PI);
+	}
+}
