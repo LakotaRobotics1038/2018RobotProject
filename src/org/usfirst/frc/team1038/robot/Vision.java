ca34d20954f28bf6dec170f3391807f4f75f3e57
Zach Simmons <zachary.j.simmons@gmail.com>
2018-01-30 17:28:04 -0500

    added turn adjust method to Vision

--- src/org/usfirst/frc/team1038/robot/Vision.java
+++ src/org/usfirst/frc/team1038/robot/Vision.java
@@ -6,12 +6,14 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Vision extends Subsystem{
+	//fields
+	public static DriveTrain robotDrive = DriveTrain.getInstance();
 	NetworkTableInstance targetSmartDash = NetworkTableInstance.getDefault();
 	NetworkTable smartDash = targetSmartDash.getTable("SmartDashBoard");   //May need slashes to denote hierarchy
 	NetworkTableEntry targetX = smartDash.getEntry("x");   //May need slashes to denote hierarchy
 	NetworkTableEntry targetY = smartDash.getEntry("y");   //May need slashes to denote hierarchy
-	double XVal = targetX.getDouble(0.0);
-	double YVal = targetY.getDouble(0.0);
+	double XVal = targetX.getDouble(-1.0);
+	double YVal = targetY.getDouble(-1.0);
 	
 	//Constructor
 	public Vision(int port) {
@@ -25,6 +27,12 @@ public class Vision extends Subsystem{
 	
 	public double getYVal() {
 		return YVal;
+	}
+	
+	public void adjustDirection() {
+		if(XVal > 0.5) {
+			robotDrive.drive(moveVal, rotateVal);
+		}
 	}
 
 	@Override
