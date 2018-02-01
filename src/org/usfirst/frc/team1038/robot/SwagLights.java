7ba5576b5cd3dc056f1f926cba0c793d569bdad0
Drew Friend <dfriend414@gmail.com>
2018-01-31 20:08:15 -0500

    Added classes foir arduino communication

---
+++ src/org/usfirst/frc/team1038/robot/SwagLights.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team1038.robot;
+
+import edu.wpi.first.wpilibj.SerialPort;
+
+public class SwagLights {
+	private SerialPort ard = new SerialPort(9600, SerialPort.Port.kMXP);
+}
