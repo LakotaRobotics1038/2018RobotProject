b5713f12b8fe96dfdc2f2e3f94ae91e97e4f6b5d
Zach Simmons <zachary.j.simmons@gmail.com>
2018-02-27 19:52:17 -0500

    jacob adding shuffleboard stuff and updated Dashboard.java testing is advised

--- src/org/usfirst/frc/team1038/robot/Dashboard.java
+++ src/org/usfirst/frc/team1038/robot/Dashboard.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc.team1038.robot;
 
+import org.usfirst.frc.team1038.auton.TurnCommand;
+import org.usfirst.frc.team1038.auton.TurnCommandPID;
 import org.usfirst.frc.team1038.subsystem.DriveTrain;
 
+import edu.wpi.cscore.HttpCamera;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Dashboard {
@@ -11,5 +14,15 @@ public class Dashboard {
 		SmartDashboard.putNumber("Gyro", I2CGyro.getInstance().getAngle());
 		SmartDashboard.putNumber("Left Encoder", DriveTrain.getInstance().getLeftDriveEncoderDistance());
 		SmartDashboard.putNumber("Right Encoder", DriveTrain.getInstance().getRightDriveEncoderDistance());
+		SmartDashboard.putNumber("Turn Command", TurnCommand.P);
+		SmartDashboard.putNumber("Turn Command", TurnCommand.I);
+		SmartDashboard.putNumber("Turn Command", TurnCommand.D);
+		SmartDashboard.getNumber("HueMax", 39);
+		SmartDashboard.getNumber("HueMin", 20);
+		SmartDashboard.getNumber("SaturationMax", 255);
+		SmartDashboard.getNumber("SaturationMin", 0);
+		SmartDashboard.getNumber("ValueMax", 255);
+		SmartDashboard.getNumber("ValueMin", 0);
+		SmartDashboard.putString("CameraPublisher","10.10.38.101:1180/?action=stream");
 	}
 }
